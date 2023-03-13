387e5955df2212a2fafcdd667d65c946abe8651e
omkarjadhav111 <123991131+omkarjadhav111@users.noreply.github.com>
2023-03-13 18:14:55 +0530

    swap two number using third variable

--- Swap.java
+++ Swap.java
@@ -1,11 +1,12 @@
-public class Swap
+public class Main
 {
 	public static void main(String[] args) {
-	int a=10,b=20;
-	a=a+b; //10+20=30
-	b=a-b;//30-20=10
-	a=a-b;//30-10=20
-	System.out.println("After swapping values are "+a+" "+b);
+	    int a=10,b=20,t;
+	    t=a;
+	    a=b;
+	    b=t;
+	    
+	    System.out.println("After swapping values are "+a+" "+b);
 	
 	}
 }
