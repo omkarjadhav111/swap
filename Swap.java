679d5fe4d7d3cda2a7dc6a22f66b2ec351b5dbe1
omkarjadhav111 <123991131+omkarjadhav111@users.noreply.github.com>
2023-03-13 18:01:02 +0530

    this is my main file

---
+++ Swap.java
@@ -0,0 +1,11 @@
+public class Swap
+{
+	public static void main(String[] args) {
+	int a=10,b=20;
+	a=a+b; //10+20=30
+	b=a-b;//30-20=10
+	a=a-b;//30-10=20
+	System.out.println("After swapping values are "+a+" "+b);
+	
+	}
+}
