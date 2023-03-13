a341201627983a9f9fcf131bf06c52a65ea7c9bd
omkarjadhav111 <123991131+omkarjadhav111@users.noreply.github.com>
2023-03-13 18:21:40 +0530

    using + and -. without using third variable.

---
+++ Swap_number.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=10,b=20,t;
+	    a=a+b;//10+20=30
+	    b=a-b;//30-20=10
+	    a=a-b;//30-10=20
+	    
+	    System.out.println("After swapping values are "+a+" "+b);
+	
+	}
+}
